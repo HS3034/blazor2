7207f6752af29248ce12608a8518b5026e0423e8
DESKTOP-D14NGSV\Windows 10 <tahzmn@gmail.com>
2020-07-03 16:29:12 +0700

    frist commit

---
+++ Model/Empoloyee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp3.Model
+{
+    public class Empoloyee
+    {
+        public int Id {get;set;}
+        public string  Name { get; set; }
+
+    }
+}
---
+++ Pages/EmployeeOverviewBase.cs
@@ -0,0 +1,32 @@
+﻿using BlazorApp3.Model;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp3.Pages
+{
+    public class EmployeeOverviewBase : ComponentBase
+    {
+        public List<Empoloyee> Empoloyees { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            Inittializeemployee();
+            return base.OnInitializedAsync();
+        }
+
+
+        public void Inittializeemployee() 
+        {
+            Empoloyees = new List<Empoloyee>() { 
+               new Empoloyee { Id = 1, Name = "Tahz" },
+                new Empoloyee { Id = 2, Name = "Smith" },
+                new Empoloyee { Id= 3,Name = "Naaan" }
+            };
+        }
+
+
+    }
+}
